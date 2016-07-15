ad629be2bb4ec818c3e0176f5b3cb2505cf9583a
SpaceForestChu <spaceforestchu@gmail.com>
2016-07-14 20:19:51 -0400

    added css show and hide buttons

--- javascript.js
+++ javascript.js
@@ -1,12 +1,14 @@
- var map;
+var map;
 
-  var markers = [];
+var markers = [];
 
   function initMap(){
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 40.7413549, lng: -73.9980244},
-      zoom: 13
+      zoom: 13,
+      mapTypeControl: false
     });
+
     var locations = [
           {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
           {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
@@ -16,16 +18,13 @@
           {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
         ];
 
+    var largeInfowindow = new google.maps.InfoWindow();
 
-    var largeInfoWindow = new google.maps.InfoWindow();
-    var bounds = new google.maps.LatLngBounds();
-
-    for( var i = 0; i < locations.length ; i++){
+    for (var i = 0; i < locations.length; i++){
       var position = locations[i].location;
       var title = locations[i].title;
 
       var marker = new google.maps.Marker({
-        map: map,
         position: position,
         title: title,
         animation: google.maps.Animation.DROP,
@@ -34,23 +33,42 @@
 
       markers.push(marker);
 
-
-      marker.addListener('click', function(){
-        populateInfoWindow(this, largeInfoWindow);
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
       });
-      bounds.extend(markers[i].position);
     }
-     map.fitBounds(bounds);
+      document.getElementById('show-listings').addEventListener('click', showListings);
+     document.getElementById('hide-listings').addEventListener('click', hideListings);
+    }
+
+   function populateInfoWindow(marker, inforwindow){
+     if( inforwindow.marker !== marker){
+       infowindow.marker = marker;
+       infowindow.setContent('<div>' + marker.title + '</div>');
+       infowindow.open(map, marker);
+
+       infowindow.addListener('closeclick', function(){
+         infowindow.marker = null;
+       });
+     }
+   }
+
+
+    function showListings(){
+      var bounds = new google.maps.LatLngBounds();
+      for( var i = 0; i < markers.length; i++){
+        markers[i].setMap(map);
+        bounds.extend(markers[i].position);
+      }
+      map.fitBounds(bounds);
+    }
+
+    function hideListings() {
+      for(var i = 0; i < markers.length; i++){
+        markers[i].setMap(null);
+      }
     }
 
 
-      function populateInforWindow(marker, infowindow) {
-        if(infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map, marker);
-          infowindow.addListener('closeclick', function(){
-            infowindow.setMarker(null);
-          });
-        }
-  }
+
+
